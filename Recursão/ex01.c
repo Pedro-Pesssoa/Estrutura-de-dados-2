c4b6ecb43953bbdfcc5ef2c3a909b4ca2244743f
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-01-31 14:39:56 -0300

    exemplo recursão

---
+++ Recursão/ex01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int n, result;
+
+    printf("\nInforme um valor -> ");
+    scanf("%i", &n);
+
+    result = fatorial(n);
+
+    printf("Fatorial de %i eh %i\n\n", n, result);
+
+    return 0;
+}
+
+int fatorial(int n){
+
+    int result;
+
+    if(n == 0){
+        result = 1;
+    }else{
+        result = n * fatorial(n-1);
+    }
+
+    return result;
+}
