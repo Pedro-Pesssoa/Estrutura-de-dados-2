701ffbb6fee9d4f7692fe540e77f32de4540ca6f
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2023-02-03 10:38:01 -0300

    problema 6

---
+++ Revisão/Problema06.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int n, result, i, cont=1;
+
+    printf("Informe um valor -> ");
+    scanf("%i", &n);
+
+    printf("\nValor -> Fatorial\n");
+    for(i=1; i<n; i++, cont++){
+        result = fatorial(cont);
+        printf("  %i         %i\n",i,result);
+    }
+    return 0;
+}
+
+int fatorial(int cont){
+
+    int result, i;
+ 
+    if(cont == 0){
+        result = 1;
+    }else{
+        result = cont * fatorial(cont-1);
+    }
+
+    return result;
+}
